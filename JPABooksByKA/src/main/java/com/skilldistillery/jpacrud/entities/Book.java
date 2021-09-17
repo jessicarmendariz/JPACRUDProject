e49c068ead9602e007f74a233473907cb61a8438
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-09-17 17:49:12 -0600

    Configure JPA

---
+++ JPABooksByKA/src/main/java/com/skilldistillery/jpacrud/entities/Book.java
@@ -0,0 +1,44 @@
+package com.skilldistillery.jpacrud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	public Book() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String name) {
+		this.title = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + "]";
+	}
+	
+	
+
+}
